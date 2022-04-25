d31c683ef79dd2814a60bfa004b716cfac8cdd5c
Gamal Abdul Hameed <87769953+Gamal-Abd-El-Hameed@users.noreply.github.com>
2022-04-25 18:44:29 +0200

    Hakaml ba3d el Sala ISA

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,7 @@
-
-// Below is a Java program to create
-// a Unit tuple from Constructor
+import java.util.List;
 
 import org.javatuples.Pair;
-import org.javatuples.Unit;
-import org.javatuples.Decade;
-
-import java.io.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
-import java.util.stream.Collectors;
+import FileReader.readHeap;
 
 public class Main {
 //    public static void main(String[] args)
@@ -30,32 +20,10 @@ public class Main {
 //        }
 //        sc.close();  //closes the scanner
 //    }
-public static void main(String[] args)
-{
-    String line = "";
-    String fileName = "F:\\heap.csv";
-    List<Pair<Integer, Integer>> x_values = new ArrayList<>();
-    try
+    public static void main(String[] args)
     {
-        BufferedReader br = new BufferedReader(new FileReader(fileName));
-        while ((line = br.readLine()) != null)   //returns a Boolean value
-        {
-            String[] values = line.split(",");
-            List<Integer> intValues = Arrays.asList(values).stream()
-                    .map(Double::parseDouble) // parse x.xxE+02 to xxx.0
-                    .map(Double::intValue) // xxx.0 to integer xxx
-                    .collect(Collectors.toList()); // back to List
-            Pair<Integer, Integer> tmp
-                    = Pair.with(intValues.get(0) + 3, intValues.get(1) + 3);
-            x_values.add(tmp);
-//            System.out.println("Employee [First Name=" + employee[0] + ", Last Name=" + employee[1] + ", Designation=" + employee[2] + "]");
-        }
-        System.out.println(x_values);
-    } catch (FileNotFoundException e) {
-        throw new RuntimeException(e);
-    } catch (IOException e)
-    {
-        e.printStackTrace();
+        String fileName = "F:\\heap.csv";
+        List<Pair<Integer, Integer>> ans = readHeap.read(fileName);
+        System.out.println(ans);
     }
-}
 }
